746b0f13389d3eb94538646c73e9623bcf295ae8
Sebastian Ernst <semo@Sebastians-MBP.home>
2016-04-11 13:52:04 -0600

    Deleted contents. Set up folder structure

--- components/home/homeCtrl.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module('quizApp');
-
-app.controller('HomeCtrl', function($scope, quizList, pastQuizList) {
-	console.log(pastQuizList)
-	$scope.quizzes = quizList;
-	$scope.pastQuizzes = pastQuizList;
-})
